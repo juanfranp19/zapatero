1e7b4d3dd54148b11eea8fac3400ff4e8617e66d
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-03-28 10:25:51 +0100

    tablas tipos_incidencias y tipo_parametros se relacionan con equipo por equipo_id

--- database/migrations/2025_03_20_000008_add_foreign_keys_in_tipos_parametros_table.php
+++ database/migrations/2025_03_20_000008_add_foreign_keys_in_tipos_parametros_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('tipos_parametros', function (Blueprint $table) {
 
-            $table->foreign('equipo_numserie')->references('numserie')->on('equipos')->onDelete('cascade');
+            $table->foreign('equipo_id')->references('id')->on('equipos')->onDelete('cascade');
         });
     }
 
