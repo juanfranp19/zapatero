65a3f8a769d319930a910ddea2c703acdef8be9d
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-04-02 09:48:27 +0200

    componentes pasados a otra rama

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,21 +1,9 @@
-import Header from './components/Header/Header.jsx'
-import PageHead from './components/PageHead/PageHead.jsx';
-import Breadcrumb from './components/Breadcrumb/Breadcrumb.jsx';
-import EstadisticasDeUso from './components/EstadisticasDeUso/EstadisticasDeUso.jsx';
-import Usos from './components/Usos/Usos.jsx';
-import Incidencias from './components/Incidencias/Incidencias.jsx';
 
 function App() {
 
     return (
         <>
-            <Header></Header>
-            <PageHead></PageHead>
-            <Breadcrumb></Breadcrumb>
 
-            <EstadisticasDeUso></EstadisticasDeUso>
-            <Usos></Usos>
-            <Incidencias></Incidencias>
         </>
     );
 }
