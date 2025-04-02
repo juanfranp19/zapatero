da733dd6fb7fa63fb7799e24f71018a791177a93
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-04-02 11:59:55 +0200

    avanzando componente Header

---
+++ react/src/components/Header/Header.jsx
@@ -0,0 +1,52 @@
+import LogoZapatero from "../LogoZapatero/LogoZapatero";
+
+const Header = () => {
+
+    return (
+        <>
+
+
+
+
+
+
+
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><LogoZapatero/></a>
+                    <button class="navbar-toggler fa-light fa-bars" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+
+
+
+
+
+
+
+
+
+        </>
+    );
+}
+
+export default Header;
---
+++ react/src/components/LogoZapatero/LogoZapatero.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/logo-zapatero.png';
+
+const LogoZapatero = () => {
+
+    return (
+        <img src={logo} alt="Zapatero a tus zapatos" />
+    );
+}
+
+export default LogoZapatero;
