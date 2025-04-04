856427af468b1884ef831f362a7dcf87da2ae51e
srpompa <jorgeaznarberenguer@gmail.com>
2025-04-04 09:00:07 +0200

    componente login terminado

--- react/src/components/Login/Login.jsx
+++ react/src/components/Login/Login.jsx
@@ -1,53 +1,37 @@
 import React from 'react';
-import './Login.css'; // Asegúrate de que tu archivo CSS esté importado
+import './Login.css';
 
-const LoginForm = () => {
-  return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100">
-      <div className="card p-4" style={{ width: '400px', borderRadius: '15px' }}>
-        <div className="card-body">
-          <h3 className="form-title text-center mb-4">Accede a tu cuenta</h3>
-          <form>
-            {/* Campo de Email con icono */}
-            <div data-mdb-input-init className="form-outline mb-4 position-relative">
-              <input
-                type="email"
-                id="form2Example1"
-                className="form-control pl-5"
-                placeholder="Email"
-              />
-              <i className="fa fa-user position-absolute" style={{ top: '50%', left: '10px', transform: 'translateY(-50%)' }}></i>
-              <label className="form-label" htmlFor="form2Example1"></label>
-            </div>
+const Login = () => {
+    return (
+        <div className="d-flex justify-content-center align-items-center min-vh-100">
+            <div className="card p-4" style={{ width: '400px', borderRadius: '1px' }}>
+                <div className="card-body">
+                    <h3 className="form-title text-center mb-4">Accede a tu cuenta</h3>
+                    <form>
+                        {/* Campo de Email con icono */}
+                        <div data-mdb-input-init className="form-outline mb-4 position-relative">
+                            <input type="email" id="form2Example1" className="form-control pl-5 custom-placeholder" placeholder="Email" style={{ paddingLeft: '30px' }}/>
+                            <i className="fa fa-user position-absolute" style={{ top: '50%', left: '10px', transform: 'translateY(-110%)'}}></i>
+                            <label className="form-label" htmlFor="form2Example1"></label>
+                        </div>
 
-            {/* Campo de Contraseña con icono */}
-            <div data-mdb-input-init className="form-outline mb-4 position-relative">
-              <input
-                type="password"
-                id="form2Example2"
-                className="form-control pl-5"
-                placeholder="Contraseña"
-              />
-              <i className="fa fa-lock position-absolute" style={{ top: '50%', left: '10px', transform: 'translateY(-50%)' }}></i>
-              <label className="form-label" htmlFor="form2Example2"></label>
-            </div>
+                        {/* Campo de Contraseña con icono */}
+                        <div data-mdb-input-init className="form-outline mb-4 position-relative">
+                            <input type="password" id="form2Example2" className="form-control pl-5 custom-placeholder" placeholder="Contraseña" style={{ paddingLeft: '30px' }}/>
+                            <i className="fa fa-lock position-absolute" style={{ top: '50%', left: '10px',transform: 'translateY(-110%)'}}></i>
+                            <label className="form-label" htmlFor="form2Example2"></label>
+                        </div>
 
-            {/* Botón de Login */}
-            <div className="d-flex justify-content-end">
-              <button
-                type="button"
-                data-mdb-button-init
-                data-mdb-ripple-init
-                className="btn btn-primary mb-3 rectangular-btn"
-              >
-                Login
-              </button>
+
+                        {/* Botón de Login */}
+                        <div className="d-flex justify-content-end">
+                            <button type="button" data-mdb-button-init data-mdb-ripple-init className="btn custom-green mb-3 rectangular-btn">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </form>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-export default LoginForm;
+export default Login;
