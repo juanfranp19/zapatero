66fe7c83f7a85c41266a6191b6eca3dbdcc69f24
srpompa <jorgeaznarberenguer@gmail.com>
2025-04-10 12:24:16 +0200

    Cambio del estilo del login para hacerlo más bonito

--- react/src/components/Login/Login.jsx
+++ react/src/components/Login/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 const Login = () => {
+    // Constante para el manejo de la visibilidad de la password
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    // Constante para modificar la visibilidad de la contraseña
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(prevState => !prevState);
+    };
+
     return (
         <div className="container">
             <div className="ring">
@@ -17,7 +25,9 @@ const Login = () => {
                 </div>
                 <div className="inputBx">
                     <i className="fa fa-lock"></i>
-                    <input type="password" placeholder="Contraseña" />
+                    <input type={passwordVisible ? "text" : "password"} placeholder="Contraseña"/>
+                    <i className={`fa ${passwordVisible ? 'fa-eye-slash' : 'fa-eye'}`} onClick={togglePasswordVisibility} // al hacer click se modifica a visible o no visible
+                    ></i>
                 </div>
                 <div className="inputBx">
                     <input type="submit" value="Login" />
