ba921ea10b457def67cf18aee181586d01f0b73c
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-04-29 10:33:49 +0200

    comentado el AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,7 +9,6 @@ use App\Models\User;
 use App\Observers\AccesoObserver;
 use App\Observers\EquipoObserver;
 use App\Observers\MovimientoObserver;
-use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,13 +27,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
-        });
+        // gates
 
         Gate::define('isAdmin', function (User $user) {
+            // devuelve true si en usuarios, la columna admin tiene valor 1 o la columna rol tiene valor "ADMIN"
             return $user->admin === 1 || $user->rol === 'ADMIN';
         });
+
+        // observadores
 
         Equipo::observe(EquipoObserver::class);
         Acceso::observe(AccesoObserver::class);
