aa5123a15287880bc11059da4be0874099f6e81d
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-05-07 10:12:43 +0200

    servicio de GET de salas y tipos de equipo y hooks

--- react/src/services/salaService.js
+++ react/src/services/salaService.js
@@ -1,9 +1,12 @@
 const API_URL = import.meta.env.VITE_API_URL;
 const API_URL_SALAS = API_URL + '/api/v1/salas';
 
-const token = localStorage.getItem('token');
-
 export const getSalas = () => {
+
+    // token del local storage
+    const token = localStorage.getItem('token');
+
+    // hace la petición a la API
     return fetch(API_URL_SALAS, {
         method: 'GET',
         headers: {
@@ -11,74 +14,15 @@ export const getSalas = () => {
         },
     })
         .then(response => {
+            // respuesta de la API
             return response.json();
         })
         .then((data) => {
+            // las salas
             return data.data;
         })
         .catch(error => {
             console.error('Error en getSalas:', error);
-            return 0;
-        });
-}
-
-export const createSala = (datosSala) => {
-    return fetch(API_URL_SALAS, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(datosSala),
-    })
-        .then(response => {
-            const data = response.json();
-            return data;
-        })
-        .catch(error => {
-            console.error('Error en createSala:', error);
-            return 0;
-        });
-}
-
-export const showSala = (salaId) => {
-    return fetch(`${API_URL_SALAS}/${salaId}`)
-        .then(response => {
-            const data = response.json();
-            return data;
-        })
-        .catch(error => {
-            console.error('Error en showSala:', error);
-            return 0;
-        });
-}
-
-export const updateSala = (salaId, datosSala) => {
-    return fetch(`${API_URL_SALAS}/${salaId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(datosSala),
-    })
-        .then(response => {
-            const data = response.json();
-            return data;
-        })
-        .catch(error => {
-            console.error('Error en updateSala:', error);
-            return 0;
-        });
-}
-
-export const deleteSala = (salaId) => {
-    return fetch(`${API_URL_SALAS}/${salaId}`, {
-        method: 'DELETE',
-    })
-        .then(response => {
-            return 'Sala eliminado correctamente';
-        })
-        .catch(error => {
-            console.error('Error en deleteSala:', error);
             return 0;
         });
 }
