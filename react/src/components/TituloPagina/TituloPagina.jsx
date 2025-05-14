4f3e85cc2b97c566364c20fba31cf659e909fdf7
srpompa <jorgeaznarberenguer@gmail.com>
2025-05-14 12:22:24 +0200

    añadir icono al TituloPagina

--- react/src/components/TituloPagina/TituloPagina.jsx
+++ react/src/components/TituloPagina/TituloPagina.jsx
@@ -4,7 +4,10 @@ const TituloPagina = (props) => {
 
     return (
         <div className="col-12 titulo-pagina">
-            <h1>{props.nombre} <span className="titulo-pagina-pequenio">Gestion de {props.nombre}</span></h1>
+            <h1>
+                <i className="bi bi-people-fill titulo-icono"></i>
+                {props.nombre} <span className="titulo-pagina-pequenio">Gestion de {props.nombre}</span>
+            </h1>
         </div>
     );
 }
