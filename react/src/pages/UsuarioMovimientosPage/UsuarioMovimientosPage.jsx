d9d3faca670eb3f9088006845726e4d439ae0bd2
srpompa <jorgeaznarberenguer@gmail.com>
2025-05-15 08:12:45 +0200

    creacion de paginas para el MenuLateral

---
+++ react/src/pages/UsuarioMovimientosPage/UsuarioMovimientosPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
+import TituloPagina from "../../components/TituloPagina/TituloPagina";
+import MenuLateral from "../../components/MenuLateral/MenuLateral";
+
+const UsuarioMovimientosPage = () => {
+  return (
+    <div className="usuario-pagina">
+      <aside className="usuario-sidebar">
+        <MenuLateral />
+      </aside>
+
+      <div className="usuario-main">
+        <header className="usuario-header">
+          <TituloPagina nombre="Usuario" />
+          <Breadcrumbs />
+        </header>
+
+      </div>
+    </div>
+  );
+};
+
+export default UsuarioMovimientosPage;
