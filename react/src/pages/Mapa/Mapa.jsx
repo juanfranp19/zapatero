9591d38f3e58b545831b9494f74b4c5f8a220d9a
srpompa <jorgeaznarberenguer@gmail.com>
2025-05-16 11:36:09 +0200

    añadir BreadCrumbs y TituloPagina

--- react/src/pages/Mapa/Mapa.jsx
+++ react/src/pages/Mapa/Mapa.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Mapa from "../../components/Mapa/Mapa.jsx";
+import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs.jsx";
+import TituloPagina from "../../components/TituloPagina/TituloPagina.jsx";
 
 const MapaPage = () => {
     return (
         <div>
+
+            <TituloPagina nombre="Mapa"/>
+
+            <Breadcrumbs/>
+
             <Mapa/>
         </div>
     );
